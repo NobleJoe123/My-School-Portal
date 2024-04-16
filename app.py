e90e2156ac0ff1208d6037f670895e2309fff74f
NobleJoe123 <145128278+NobleJoe123@users.noreply.github.com>
2024-04-16 13:52:10 +0100

    add

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request, url_for, session
+from datetime import datetime
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 
@@ -22,8 +23,11 @@ mysql = MySQL(app)
 def index():
     return render_template('index.html')
 
-
-
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        return redirect(url_for('home'))
+    return render_template('index.html')
 
 @app.route('/about', methods=['GET', 'POST'])
 def about():
@@ -32,28 +36,65 @@ def about():
     return render_template('about.html')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+     
+ 
+@app.route('/reg', methods=['GET', 'POST'])
+def reg():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor()
+        cursor.execute('INSERT INTO reg VALUES (NULL, %s, %s, %s)', (username, password, email,))
+        mysql.connection.commit()
+        msg = 'You have successfully registered!'
+        return render_template('login.html', msg=msg)
+    elif request.method == 'POST':
+        msg = 'Please fill out the form!'
+    return render_template('register.html', msg=msg)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     msg = ''
-    if request.method == 'POST' and 'Username' in request.form and 'Password' in request.form:
-        Username = request.form['Username']
-        Password = request.form['Password']
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM register WHERE Username = % s AND Password = % s', (Username, Password, ))
+        cursor.execute('SELECT * FROM reg WHERE username = % s AND password = % s', (username, password, ))
         account = cursor.fetchone()
         if account:
             session['loggedin'] = True
             session['Id'] = account['id']
             session['Username'] = account['Username']
             msg = 'Logged in successfully !'
-            return render_template('contact.html', msg = 'Username')
+            return render_template('contact.html', msg = 'username')
         else:
             msg = 'Incorrect username / password !'
     return render_template('login.html', msg = msg)
 
 
 
+@app.route('/enrol', methods=['GET', 'POST'])
+def enrol():
+    msg = ''
+    if request.method == 'POST' and 'studentname' in request.form and 'parentmobile' in request.form and 'formerclass' in request.form and 'address' in request.form:
+        studentname = request.form['studentname']
+        parentmobile = request.form['parentmobile']
+        formerclass = request.form['formerclass']
+        address = request.form['address']
+        cursor = mysql.connection.cursor()
+        cursor.execute('INSERT INTO enrol VALUES (NULL, %s, %s, %s, %s)', (studentname, parentmobile,formerclass,address))
+        mysql.connection.commit()
+        msg = 'You are successfully a student!'
+        return render_template('login.html', msg=msg)
+    elif request.method == 'POST':
+        msg = 'Please fill out the form!'
+    return render_template('enrol.html', msg=msg)
 
 
 if __name__ == '__main__':
